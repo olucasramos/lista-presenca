57f0f945bf04670538a25cbfc45b7f35e6e7e863
olucasramos <lucas.ramos745@gmail.com>
2025-08-09 11:06:04 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Importa os módulos do Firebase (versão modular)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getDatabase, ref, set, get, child, update, remove } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyB7bfyqvuKEPjC-Wchk2qoE6cEHz94Ln3s",
+    authDomain: "controle-de-presencas.firebaseapp.com",
+    databaseURL: "https://controle-de-presencas-default-rtdb.firebaseio.com",
+    projectId: "controle-de-presencas",
+    storageBucket: "controle-de-presencas.firebasestorage.app",
+    messagingSenderId: "329637142585",
+    appId: "1:329637142585:web:0ee7c006c0091767926de8"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Exporta funções e variáveis para uso em outros arquivos
+export { db, ref, set, get, child, update, remove };
